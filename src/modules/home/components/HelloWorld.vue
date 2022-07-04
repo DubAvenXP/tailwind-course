6647b68c14745f417e7a8ac85d61d977a339ee46
Alejandro Dubon <51029575+DubAvenXP@users.noreply.github.com>
2022-07-04 10:34:46 -0600

    Initial commit

---
+++ src/modules/home/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useExampleStore } from "@/stores"
+
+const store = useExampleStore()
+</script>
+
+<template>
+	<h1>{{ store.getMessage }}</h1>
+</template>
+
+<style lang="sass">
+h1
+	color: red
+</style>
